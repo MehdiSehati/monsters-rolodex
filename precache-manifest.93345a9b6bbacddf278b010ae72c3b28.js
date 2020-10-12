a922d77ec9050798ca4f7a8282db346f94fa29bf
MehdiSehati <sehati.mehdi@gmail.com>
2020-10-12 11:00:33 +0330

    Updates

---
+++ precache-manifest.93345a9b6bbacddf278b010ae72c3b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720e3294a09d731201a167ac81d7ef49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b14380bd1aacf78cbef",
+    "url": "/monsters-rolodex/static/css/main.a94a420e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b14380bd1aacf78cbef",
+    "url": "/monsters-rolodex/static/js/main.945d6656.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
